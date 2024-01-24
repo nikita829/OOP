3e5053b34b76f26d61992f92b1df655c567ef799
nikita829 <nikita96587@gmail.com>
2024-01-24 12:36:54 +0500

    2

--- ЛР5/Form1.Designer.cs
+++ ЛР5/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace ЛР5
+﻿namespace ЛР_5___3
 {
     partial class Form1
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Освободить все используемые ресурсы.
+        ///  Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,11 +20,11 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 393);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -44,4 +45,3 @@
         #endregion
     }
 }
-
--- ЛР5/Form1.cs
+++ ЛР5/Form1.cs
@@ -1,101 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
+using System.Drawing;
 
-namespace ЛР5
+namespace ЛР_5___3
 {
-    //public partial class Form1 : Form
-    //{
-    //    List<Panel> p = new List<Panel>();
-    //    public Form1()
-    //    {
-    //        InitializeComponent();
-    //        for (int i = 0; i < 3; i++)
-    //        {
-    //            p.Add(new Panel()
-    //            {
-    //                Location = new Point(50 + 150 * i, 50),
-    //                Size = new Size(100, 100),
-    //                BackColor = Color.Green,
-    //                Tag = i,
-    //            });
-    //            Controls.Add(p[i]);
-    //            p[i].MouseClick += PanelClick;
-    //        }
-    //        InitParams();
-    //    }
-    //    private void PanelClick(object s, EventArgs e)
-    //    {
-    //        int tag = (int)((Panel)s).Tag;
-    //        int control = tag + 1;
-    //        if (control == 3) control = 0;
-    //        p[control].Controls.Add(AddedControls(tag));
-    //    }
-    //    Size size; Point point;
-    //    private void InitParams()
-    //    {
-    //        size = new Size(p[1].Width / 2, p[1].Height / 2);
-    //        point = new Point(p[1].Width / 4, p[1].Height / 4);
-    //    }
-    //    private Control AddedControls(int tag)
-    //    {
-    //        switch (tag)
-    //        {
-    //            case 0:
-    //                return new Button() { Size = size, Location = point };
-    //            case 1:
-    //                return new TextBox() { Size = size, Location = point };
-    //            default:
-    //                return new Label() { Size = size, Location = point, Text = "Привет!" };
-    //        }
-    //    }
-    //}
-    public class MainForm : Form
+    public partial class Form1 : Form
     {
-        private Panel panel;
-
-        public MainForm()
-        {
-            panel = new Panel();
-            panel.Dock = DockStyle.Fill;
-
-            panel.Click += Panel_Click;
-            this.Click += MainForm_Click;
-
-            this.Controls.Add(panel);
-        }
-
-        private void Panel_Click(object sender, EventArgs e)
-        {
-            Button button = new Button();
-            button.Text = "Button";
-            button.Click += Button_Click;
-
-            panel.Controls.Add(button);
-        }
-
-        private void Button_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Button clicked!");
-        }
-
         private void MainForm_Click(object sender, EventArgs e)
         {
             TextBox textBox = new TextBox();
-
             panel.Controls.Add(textBox);
+            textBox.Location = new Point(157, 100);
+        }
+        public Form1() 
+        { 
+            InitializeComponent(); 
         }
 
-        [STAThread]
-        static void Main()
+        private void Form1_Load(object sender, EventArgs e)
         {
-            Application.Run(new MainForm());
+            panel = new Panel();
+            panel.BackColor = Color.HotPink;
+            Controls.Add(panel); panel.Location = new Point(10, 10); panel.Size = new Size(450, 300);
+            panel.Click += Panel_Click;
+            Click += MainForm_Click;
+        }
+        private Panel panel;
+        private void Panel_Click(object sender, EventArgs e)
+        {
+            Button button = new Button(); button.Text = "Кнопка"; button.Click += Button_Click;
+
+            panel.Controls.Add(button);
+            button.BackColor = Color.LightBlue;
+            button.Location = new Point(170, 150);
+        }
+
+        private void Button_Click(object sender, EventArgs e) 
+        { 
+            MessageBox.Show("Вы нажали на кнопку!"); 
         }
     }
 }
